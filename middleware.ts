0cbf30b3e711a2f8c76d5dc82bb8ab916e4b657b
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-11 10:32:43 +0900

    ミドルウェア演習結果修正

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import withAuth from "next-auth/middleware";
 /**
  * 演習 7-6 ミドルウェアを実装する
  * 商品管理系のすべてのページをガード対象とする
+ * ユーザー登録画面も認証必須とするためガード対象とする
  */
 // ミドルウェアの処理と、未ログイン時のリダイレクト先を指定
 export default withAuth({
@@ -12,5 +13,9 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/api/products/:path*"],
+  // 配列に"/api/products/:path*"と"/api/users/:path*"を追加
+  matcher: [
+    "/api/products/:path*",
+    "/api/users/:path*"
+  ],
 };
